af3d0ddead703bad27dbe78fa4e38f5fe861afc7
Tran Viet Anh <tran@vietanh.cz>
2022-11-27 16:05:25 +0100

    Create products list filtering and tabbing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react'
-import { Box, CircularProgress, Container, Typography } from '@mui/material'
+import { Alert, Box, CircularProgress, Container, Typography } from '@mui/material'
 import { ProductsList } from './components/ProductsList'
-import { loadAlzaData } from './data'
+import { loadAlzaData } from './dataFetch/getData'
 import { CategoriesList } from './components/CategoriesList'
 import { CarouselPanel } from './components/CarouselPanel'
+import { ProductsListFilter } from "./components/ProductsListFilter"
 
 function App () {
   const [products, setProducts] = useState<Array<any>>([])
-  const [fail, setFail] = useState('')
+  const [failedToLoad, setFailedToLoad] = useState('')
 
   useEffect(() => {
     loadAlzaData().then(data =>
       setProducts(data.data)
     ).catch(err => {
-      setFail('Načtení produktů se nepodařilo :(')
-      throw err
+      setFailedToLoad('Načtení produktů se nepodařilo :(')
+      throw Error('Products failed to load.')
     })
   }, [])
 
@@ -33,20 +34,30 @@ function App () {
     'Příslušenství',
   ]
 
+  const filters = {
+    top: 'TOP',
+    topSellers: 'Nejprodávanější',
+    priceLowToHigh: 'Od nejlevnějšího',
+    priceHighToLow: 'Od nejdražšího'
+  }
+
+  console.log(products)
+
   return (
     <>
       <Container>
-        <Typography variant='h5' gutterBottom>Notebooky</Typography>
+        <Typography variant='h5' gutterBottom color='primary'>Notebooky</Typography>
         <CategoriesList categories={ntbCategories}/>
-        <Typography variant='h5' gutterBottom>Nejprodávanější</Typography>
-        <CarouselPanel slides={products} />
-        {!fail
+        <Typography variant='h5' gutterBottom color='primary'>Nejprodávanější</Typography>
+        <CarouselPanel slides={products}/>
+        {!failedToLoad
           ? products.length
-            ? <ProductsList products={products}/>
-            : <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                <CircularProgress sx={{display: 'flex', justifyContent: 'center'}} />
-              </Box>
-          : <p>{fail}</p>
+            // ? <ProductsList products={products}/>
+            ? <ProductsListFilter filters={filters} products={products}/>
+            : <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+              <CircularProgress sx={{ display: 'flex', justifyContent: 'center' }}/>
+            </Box>
+          : <Alert severity="error">Načtení produktů se nezdařilo :(</Alert>
         }
       </Container>
     </>
---
+++ src/components/ProductsListFilter.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import { Divider, Tab, Tabs } from '@mui/material'
+import { isEmpty } from 'lodash'
+import { ProductsList } from './ProductsList'
+import { styles } from './ProductsListFilter.style'
+
+const listFilter = (list: any[], criterium: string) => {
+  switch (criterium) {
+    case 'top':
+      return [...list].sort((a, b) => b.rating - a.rating)
+    case 'priceLowToHigh':
+      return [...list].sort((a, b) => a.priceInfo.priceNoCurrency - b.priceInfo.priceNoCurrency)
+    case 'priceHighToLow':
+      return [...list].sort((a, b) => b.priceInfo.priceNoCurrency - a.priceInfo.priceNoCurrency)
+    default:
+      return list
+  }
+}
+
+interface Props {
+  filters: { [key: string]: string }
+  products: any[]
+}
+
+export const ProductsListFilter = ({ filters, products }: Props) => {
+  const [selectedTab, setSelectedTab] = useState<number>(0)
+  const handleChange = (event: React.SyntheticEvent, newSelectedTab: number) => {
+    setSelectedTab(newSelectedTab)
+  }
+
+  return (
+    <>
+      <Tabs value={selectedTab} onChange={handleChange}>
+        {!isEmpty(filters) && Object.values(filters).map((value, i) => (
+          <Tab label={value} key={i} sx={styles.tab}/>
+        ))}
+      </Tabs>
+      <Divider />
+      {!isEmpty(filters) && Object.keys(filters).map((key, i) => (
+        selectedTab === i && <ProductsList products={listFilter(products, key)} key={i}/>
+      ))}
+    </>
+  )
+}
